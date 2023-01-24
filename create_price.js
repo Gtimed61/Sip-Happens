45266fabe1ae6d135001bd1c4295dea58641787c
TyFB <tfb1223@gmail.com>
2023-01-24 15:11:36 -0600

    merging develop

--- create_price.js
+++ create_price.js
@@ -1,8 +1,8 @@
 const stripe = require('stripe')('sk_test_51MS9LnLPqkfbw31McDXZ7mjsVzdASliF9TPIw3ArHBHHbOUE6ZaAbBiLrnuU1uALKeNEVjaSu2tmhZjzDaMxq3aX00csRD77XI');
 
 stripe.products.create({
-  name: 'Starter Subscription',
-  description: '$12/Month subscription',
+  name: 'Iced Coffee',
+  description: 'Iced Coffee with ',
 }).then(product => {
   stripe.prices.create({
     unit_amount: 1200,
